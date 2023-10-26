01e66f82db54fc5122c171f20ed6dd3df3cfa7c5
Jorexbp <jorgebp2004@gmail.com>
2023-10-26 19:13:30 +0200

    meme

--- Ficheros_C/Estructuras/alterMem/modMem.c
+++ Ficheros_C/Estructuras/alterMem/modMem.c
@@ -16,7 +16,12 @@ void mostrarSiguientePosicionMemoria(int n)
 
     int *pValor = &n + sizeof(n);
     *pValor = 15;
+    int *pN = &n;
+    *pN = *pValor; // Sobreescribo
     printf("\nPosicion en memoria: %p", pValor);
-    printf("\nValor en memoria: %d", *pValor);
+    printf("\nValor en memoria: %d\n", *pValor);
+
+    printf("Valor de la variable: %d\n",n);
+    
 
 }
