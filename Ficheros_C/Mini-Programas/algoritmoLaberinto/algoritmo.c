cfa5891fb5c55e2e48ec0b541945d6693fc235c6
jorexbp <jorgebp2004@gmail.com>
2023-10-31 10:33:32 -0200

    algor

--- Ficheros_C/Mini-Programas/algoritmoLaberinto/algoritmo.c
+++ Ficheros_C/Mini-Programas/algoritmoLaberinto/algoritmo.c
@@ -3,16 +3,16 @@
 #define ANCHURA 12
 #define ALTURA 12
 
-void mazeTraverse(char [][ANCHURA], int, int);
+void mazeTraverse(char[][ANCHURA], int, int);
 void mostrarLaberinto(char[][ANCHURA]);
 
- char laberinto[ALTURA][ANCHURA] = {
+char laberinto[ALTURA][ANCHURA] = {
 
     {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
     {'#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '#'},
     {'.', '.', '#', '.', '#', '.', '#', '#', '#', '#', '.', '#'},
     {'#', '#', '#', '.', '#', '.', '.', '.', '.', '#', '.', '#'},
-    {'#', '.', '.', '.', '.', '#', '#', '#', '.', '#', '.', '.'},
+    {'#', '.', '.', '.', '.', '#', '#', '#', '.', '#', '.', 'E'},
     {'#', '#', '#', '#', '.', '#', '.', '#', '.', '#', '.', '#'},
     {'#', '.', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#'},
     {'#', '#', '.', '#', '.', '#', '.', '#', '.', '#', '.', '#'},
@@ -23,20 +23,76 @@ void mostrarLaberinto(char[][ANCHURA]);
 
 };
 
+const char direcciones[] = {'A', 'B', 'D', 'I'}; // ARRIBA, ABAJO, DRCH, IZQ -> PREFERNCIA
 int main()
 {
     int colStart = 0;
     int rowStart = 2;
+    char preferencia = direcciones[2];
     mostrarLaberinto(laberinto);
-     mazeTraverse(laberinto,rowStart,colStart);
+    mazeTraverse(laberinto, rowStart, colStart);
     return 0;
 }
 
 void mazeTraverse(char lab[][ANCHURA], int rowIni, int colIni)
 {
-      lab[rowIni][colIni] = 'X';
+    int rowPos = rowIni;
+    int colPos = colIni;
+
+    lab[rowPos][colPos] = 'X';
+
+    int i = 5;
+    // lab[rowPos][colPos] != 'E'
+    while (lab[rowPos][colPos] != 'E')
+    {
         mostrarLaberinto(lab);
-    
+
+        // Comprobacion de salida adyacente
+        if (lab[rowPos][colPos - 1] == 'E')
+        {
+            colPos--;
+        }
+        else if (lab[rowPos - 1][colPos] == 'E')
+        {
+            rowPos--;
+        }
+        else if (lab[rowPos + 1][colPos] == 'E')
+        {
+            rowPos++;
+        }
+        else if (lab[rowPos][colPos + 1] == 'E')
+        {
+            colPos++;
+        }
+
+        // Movimientos
+        else if (lab[rowPos + 1][colPos] != '#' && lab[rowPos + 1][colPos] != 'X')
+        {
+            rowPos++;
+            lab[rowPos][colPos] = 'X';
+        }
+        else if (lab[rowPos][colPos + 1] != '#' && lab[rowPos][colPos + 1] != 'X')
+        {
+            colPos++;
+            lab[rowPos][colPos] = 'X';
+        }
+        else if (lab[rowPos - 1][colPos] != '#' && lab[rowPos - 1][colPos] != 'X')
+        {
+            rowPos--;
+            lab[rowPos][colPos] = 'X';
+        }
+        else if (lab[rowPos][colPos - 1] != '#' && lab[rowPos][colPos - 1] != 'X')
+        {
+            colPos--;
+            lab[rowPos][colPos] = 'X';
+        }
+      
+       
+
+        i--;
+    }
+
+    printf("I: %d", i);
 }
 
 void mostrarLaberinto(char lab[][ANCHURA])
@@ -44,9 +100,10 @@ void mostrarLaberinto(char lab[][ANCHURA])
     for (int i = 0; i < ALTURA; i++)
     {
         for (int j = 0; j < ANCHURA; j++)
-        {         
+        {
             printf("%c ", lab[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
---
+++ Ficheros_C/Mini-Programas/algoritmoLaberinto/guiri/g.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mazePrint(char *maze[12][12]);
+void mazeTraversal(char *maze[12][12]);
+static int face = 6;
+
+int main()
+{
+    int i = 0;
+    int j = 0; 
+    int k = 0;
+    int start;
+    int xpos;
+
+    char *mazeone[12][12] = {
+       //0///1///2///3///4///5///6///7///8///9///10//11///
+        "#","#","#","#","#","#","#","#","#","#","#","#",//0
+        "#",".",".",".","#",".",".",".",".",".",".","#",//1
+        ".",".","#",".","#",".","#","#","#","#",".","#",//2
+        "#","#","#",".","#",".",".",".",".","#",".","#",//3
+        "#",".",".",".",".","#","#","#",".","#",".",".",//4
+        "#","#","#","#",".","#",".","#",".","#",".","#",//5
+        "#","#",".","#",".","#",".","#",".","#",".","#",//6
+        "#","#",".","#",".","#",".","#",".","#",".","#",//7
+        "#",".",".",".",".",".",".",".",".","#",".","#",//8
+        "#","#","#","#","#","#",".","#","#","#",".","#",//9
+        "#",".",".",".",".",".",".","#",".",".",".","#",//10
+        "#","#","#","#","#","#","#","#","#","#","#","#",};//11
+
+    for (i = 0; i <12; i++)
+        if (mazeone[i][0] == "." ) {
+            start = i; 
+            mazeone[start][0] = "x";
+            xpos = start;
+            break;
+        }
+
+    printf("X is the starting point.\n");
+    printf("Press Space Bar to watch the X move.\n\n\n");
+    getchar();
+    mazePrint(mazeone);
+    getchar();
+    return 0;
+}
+
+void mazePrint(char *maze[12][12])
+{   
+    int x = 0; 
+    int y = 0;
+
+    system("cls");
+    for (x = 0; x < 12; x++) {
+        for (y = 0; y < 12; y++) {
+            printf("%s ", maze[x][y]);
+        }
+        printf("\n"); 
+    }
+    getchar(); 
+    mazeTraversal(maze);
+}
+
+void mazeTraversal(char *maze[12][12])
+{
+    int x = 0; 
+    int y = 0;
+
+    for (x = 0; x < 12; x++) {
+        for (y = 0; y < 12; y++) {
+            if (maze[y][x] == "x")
+                break;
+        } 
+        if(maze[y][x] == "x")
+            break;
+    }
+
+    for (y = 0; y < 12; y++) {
+        for (x = 0; x < 12; x++) {
+            if (maze[y][x] == "x")
+                break;
+        } 
+        if (maze[y][x] == "x")
+            break;
+    }
+
+    maze[y][x] = ".";
+
+    switch (face) {
+        case 6:{
+            if (maze[y][x-1] == ".") {
+                maze[y][x-1] = "x"; 
+                face = 5;
+            } else if (maze[y + 1][x] == ".") {
+                maze[y + 1][x] = "x"; 
+                face = 6;
+            } else if (maze[y][x+1] == ".") {
+                maze[y][x+1] = "x";
+                face = 8;
+            } else if (maze[y - 1][x] == ".") {
+                maze[y - 1][x] = "x"; 
+                face = 4;
+            }
+             break;
+        }
+        case 8:{
+            if (maze[y + 1][x] == ".") {
+                maze[y + 1][x] = "x"; 
+                face = 6;
+            } else if (maze[y][x+1] == ".") {
+                maze[y][x+1] = "x";
+                face = 8;
+            } else if (maze[y - 1][x] == ".") {
+                maze[y - 1][x] = "x"; 
+                face = 4;
+            } else if (maze[y][x-1] == ".") {
+                maze[y][x-1] = "x"; 
+                face = 5;
+            }
+             break;
+        }
+        case 4:{
+            if (maze[y][x+1] == ".") {
+                maze[y][x+1] = "x";
+                face = 8;
+            } else if (maze[y - 1][x] == ".") {
+                maze[y - 1][x] = "x"; 
+                face = 4;
+            } else if (maze[y][x-1] == ".") {
+                maze[y][x-1] = "x"; 
+                face = 5;
+            } else if (maze[y + 1][x] == ".") {
+                maze[y + 1][x] = "x"; 
+                face = 6;
+            }
+             break;
+        }
+        case 5:{
+            if (maze[y - 1][x] == ".") {
+                maze[y - 1][x] = "x";
+                face = 4;
+            } else if (maze[y][x-1] == ".") {
+                maze[y][x-1] = "x"; 
+                face = 5;
+            } else if (maze[y + 1][x] == ".") {
+                maze[y + 1][x] = "x"; 
+                face = 6;
+            } else if (maze[y][x+1] == ".") {
+                maze[y][x+1] = "x";
+                face = 8;
+            }
+             break;
+        }
+    }
+
+    mazePrint(maze);
+}
