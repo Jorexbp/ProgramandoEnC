552ba459a2df69ede485b9a6263e7f91d3a8847f
Jorexbp <jorgebp2004@gmail.com>
2023-11-08 16:55:25 +0100

    Stack implementado

--- Ficheros_C/Estructuras/Stacks/Stacks.c
+++ Ficheros_C/Estructuras/Stacks/Stacks.c
@@ -9,24 +9,45 @@ struct nodo
 // Push al top
 // Pop del top
 
-
-struct nodo * push(struct nodo *, int);
-int pop(struct nodo *);
+struct nodo *push(struct nodo *, int);
+struct nodo *pop(struct nodo *);
 void printStack(struct nodo *);
 int main()
 {
 
-   struct nodo *topPtr;
-        
-  topPtr = push(topPtr, 10);
-   topPtr =  push(topPtr,25);
+    struct nodo *topPtr;
 
-    printStack(topPtr);
+  
+    
+    int usuario = 0;
 
+    do
+    {
+        printf("%s", "Introduzca una opcion:\n1- Push valor\n2- Pop valor\n3- Salir\n");
+        printStack(topPtr);
+            
+        scanf("%d", &usuario);
+        switch (usuario)
+        {
+        case 1:
+            printf("Introduzca un numero\n");
+            int n = 0;
+            scanf("%d", &n);
+            topPtr = push(topPtr, n);
+            break;
+        case 2:
+           topPtr = pop(topPtr);
+            break;
+        case 3:
+        break;
+        default:
+            printf("Valor no valido\n");
+        }
+    } while (usuario != 3);
     return 0;
 }
 
-struct nodo *  push(struct nodo *topPtr, int data)
+struct nodo *push(struct nodo *topPtr, int data)
 {
     //  (struct nodo *)
     struct nodo *nuevo = malloc(sizeof(struct nodo));
@@ -36,8 +57,17 @@ struct nodo *  push(struct nodo *topPtr, int data)
         nuevo->nextNodo = topPtr;
         topPtr = nuevo;
         return topPtr;
-       
     }
+}
+struct nodo *pop(struct nodo *topPtr)
+{
+    if(topPtr != NULL){
+    printf("%s%d\n","Valor popeado: ",topPtr->data);
+    topPtr = topPtr->nextNodo;
+    }else{
+        printf("%s","//////////////\nNO EXISTEN VALORES\n//////////////\n");
+    }
+    return topPtr;
 }
 
 void printStack(struct nodo *Stack)
@@ -46,9 +76,8 @@ void printStack(struct nodo *Stack)
     printf("\n");
     while (Stack != NULL)
     {
-        printf("%d ---> ",Stack->data);
+        printf("%d ---> ", Stack->data);
         Stack = Stack->nextNodo;
-        
     }
-    printf("%s","NULL\n");
+    printf("%s", "NULL\n");
 }
