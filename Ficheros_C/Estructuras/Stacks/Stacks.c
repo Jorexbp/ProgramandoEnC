42612d6f69e042ae639821fd06075e9a81a2ae8b
Jorexbp <jorgebp2004@gmail.com>
2023-11-08 17:03:14 +0100

    Doc stack

--- Ficheros_C/Estructuras/Stacks/Stacks.c
+++ Ficheros_C/Estructuras/Stacks/Stacks.c
@@ -3,29 +3,28 @@
 
 struct nodo
 {
-    int data;
-    struct nodo *nextNodo;
+    int data;              // Valor del nodo
+    struct nodo *nextNodo; // Ptr al siguiente nodo
 };
-// Push al top
-// Pop del top
 
-struct nodo *push(struct nodo *, int);
-struct nodo *pop(struct nodo *);
+struct nodo *push(struct nodo *, int); // Push al top
+struct nodo *pop(struct nodo *);       // Pop del top
 void printStack(struct nodo *);
 int main()
 {
 
-    struct nodo *topPtr;
+    struct nodo *topPtr; // Primero nodo NULL
 
-  
-    
     int usuario = 0;
+
+    // Esto es un Stack por lo que solo se puede acceder por el ultimo nodo (FIFO),
+    // Añades un nuevo nodo e irá al inicio e igual si popeas un nodo, será el primero
 
     do
     {
         printf("%s", "Introduzca una opcion:\n1- Push valor\n2- Pop valor\n3- Salir\n");
         printStack(topPtr);
-            
+
         scanf("%d", &usuario);
         switch (usuario)
         {
@@ -36,10 +35,10 @@ int main()
             topPtr = push(topPtr, n);
             break;
         case 2:
-           topPtr = pop(topPtr);
+            topPtr = pop(topPtr);
             break;
         case 3:
-        break;
+            break;
         default:
             printf("Valor no valido\n");
         }
@@ -50,22 +49,25 @@ int main()
 struct nodo *push(struct nodo *topPtr, int data)
 {
     //  (struct nodo *)
-    struct nodo *nuevo = malloc(sizeof(struct nodo));
-    if (nuevo != NULL)
+    struct nodo *nuevo = malloc(sizeof(struct nodo)); // Espacio en memoria para el nodo
+    if (nuevo != NULL)                                // Si se ha podido alocar en memoria
     {
-        nuevo->data = data;
-        nuevo->nextNodo = topPtr;
-        topPtr = nuevo;
+        nuevo->data = data;       // Al nuevo nodo le pongo los datos
+        nuevo->nextNodo = topPtr; // El nuevo nodo apunta al anterior top del Stack
+        topPtr = nuevo;           // El nuevo nodo se convierte en el top del Stack
         return topPtr;
     }
 }
 struct nodo *pop(struct nodo *topPtr)
 {
-    if(topPtr != NULL){
-    printf("%s%d\n","Valor popeado: ",topPtr->data);
-    topPtr = topPtr->nextNodo;
-    }else{
-        printf("%s","//////////////\nNO EXISTEN VALORES\n//////////////\n");
+    if (topPtr != NULL)
+    {                                                      // Si el nodo no es NULL
+        printf("%s%d\n", "Valor popeado: ", topPtr->data); // Antes de popear muestro la info quitada
+        topPtr = topPtr->nextNodo;                         // Para quitar un nodo, muevo el top del Stack al puntero del siguiente nodo del top
+    }
+    else
+    {
+        printf("%s", "//////////////\nNO EXISTEN VALORES\n//////////////\n");
     }
     return topPtr;
 }
@@ -74,10 +76,10 @@ void printStack(struct nodo *Stack)
 {
     int pos = 1;
     printf("\n");
-    while (Stack != NULL)
+    while (Stack != NULL) // Mientras que haya nodos
     {
-        printf("%d ---> ", Stack->data);
-        Stack = Stack->nextNodo;
+        printf("%d ---> ", Stack->data); // Muestra la info del nodo
+        Stack = Stack->nextNodo; // Para al siguiente nodo mediante el puntero
     }
     printf("%s", "NULL\n");
 }
