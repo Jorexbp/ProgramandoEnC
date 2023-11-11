ff0b5d224bd686b81609a0c28fabbde3f9369800
Jorexbp <jorgebp2004@gmail.com>
2023-11-11 11:54:24 +0100

    Queue acabado

--- Ficheros_C/Estructuras/Queues/Queue.c
+++ Ficheros_C/Estructuras/Queues/Queue.c
@@ -9,22 +9,38 @@ struct nodo
 
 void add(struct nodo *, int); // Push al bottom
 void pop(struct nodo *);      // Pop del top
-void printStack(struct nodo *);
+void printQueue(struct nodo *);
 int main()
 {
     struct nodo *topPtr = (struct nodo *)malloc(sizeof(struct nodo));
-
-    topPtr->data = 42;
+    topPtr->data = NULL;
     topPtr->nextNodo = NULL;
 
-    add(topPtr, 10);
 
-    add(topPtr, 25);
-    add(topPtr, 97);
+int usuario = 0;
+     do
+    {
+        printf("%s", "Introduzca una opcion:\n1- Push valor\n2- Pop valor\n3- Salir\n");
+        printQueue(topPtr);
 
-    printStack(topPtr);
-    pop(topPtr);
-    printStack(topPtr);
+        scanf("%d", &usuario);
+        switch (usuario)
+        {
+        case 1:
+            printf("Introduzca un numero\n");
+            int n = 0;
+            scanf("%d", &n);
+            add(topPtr, n);
+            break;
+        case 2:
+            pop(topPtr);
+            break;
+        case 3:
+            break;
+        default:
+            printf("Valor no valido\n");
+        }
+    } while (usuario != 3);
 
     return 0;
 }
@@ -37,11 +53,14 @@ void add(struct nodo *current, int valor)
     temp->data = valor;    // En la temporal asigno el valor
     temp->nextNodo = NULL; // Como va a ser el ultimo nodo de la LinkedList no apunta a nada
 
+
+
     while (current->nextNodo != NULL) // Recorro todo el LinkedList hasta encontrar un enlace que sea NULL
     {
         current = current->nextNodo; // Una vez encontrado, asigna el nodo al nodo que tiene el link NULL
     }
     current->nextNodo = temp; // A ese nodo con el link NULL, le cambio el enlace por la direccion de la variable temporal que ya tiene los datos
+
 }
 void pop(struct nodo *current)
 {
@@ -72,14 +91,14 @@ void pop(struct nodo *current)
     }
 }
 
-void printStack(struct nodo *Stack)
+void printQueue(struct nodo *Stack)
 {
     int pos = 1;
     printf("\n");
     printf("%s", "NULL ");
-    while (Stack != NULL) // Mientras que haya nodos
+    while (Stack != NULL && Stack->nextNodo != 0) // Mientras que haya nodos
     {
-        printf("<--- %d ", Stack->data); // Muestra la info del nodo
+        printf("<--- %d ", Stack->nextNodo->data); // Muestra la info del nodo
         Stack = Stack->nextNodo;         // Para al siguiente nodo mediante el puntero
     }
 
