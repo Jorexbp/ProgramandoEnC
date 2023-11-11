47a80a2fe23ae1bb7e4a5d409b152d8ea6857436
Jorexbp <jorgebp2004@gmail.com>
2023-11-11 13:36:57 +0100

    Falta arreglar queue add

--- Ficheros_C/Estructuras/Queues/Queue.c
+++ Ficheros_C/Estructuras/Queues/Queue.c
@@ -7,44 +7,51 @@ struct nodo
     struct nodo *nextNodo; // Ptr al siguiente nodo
 };
 
-void add(struct nodo *, int); // Push al bottom
+void add(struct nodo **, int); // Push al bottom
 void pop(struct nodo **);      // Pop del top
 void printQueue(struct nodo *);
 int main()
 {
-    struct nodo *topPtr = (struct nodo *)malloc(sizeof(struct nodo));
-    topPtr->nextNodo = NULL;
+    struct nodo *topPtr = NULL;
 
+    add(&topPtr, 1);
 
-int usuario = 0;
-     do
-    {
-        printf("%s", "Introduzca una opcion:\n1- Push valor\n2- Pop valor\n3- Salir\n");
-        printQueue(topPtr);
+    add(&topPtr, 2);
 
-        scanf("%d", &usuario);
-        switch (usuario)
+    add(&topPtr, 3);
+    printQueue(topPtr);
+
+    /*
+    int usuario = 0;
+         do
         {
-        case 1:
-            printf("Introduzca un numero\n");
-            int n = 0;
-            scanf("%d", &n);
-            add(topPtr, n);
-            break;
-        case 2:
-            pop(&topPtr);
-            break;
-        case 3:
-            break;
-        default:
-            printf("Valor no valido\n");
-        }
-    } while (usuario != 3);
+            printf("%s", "Introduzca una opcion (Queue):\n1- Push valor\n2- Pop valor\n3- Salir\n");
+            printQueue(topPtr);
+
+            scanf("%d", &usuario);
+            switch (usuario)
+            {
+            case 1:
+                printf("Introduzca un numero\n");
+                int n = 0;
+                scanf("%d", &n);
+                add(&topPtr, n);
+                break;
+            case 2:
+                pop(&topPtr);
+                break;
+            case 3:
+                break;
+            default:
+                printf("Valor no valido\n");
+            }
+        } while (usuario != 3);
+        */
 
     return 0;
 }
 
-void add(struct nodo *current, int valor)
+void add(struct nodo **current, int valor)
 {
     struct nodo *temp;
     temp = (struct nodo *)malloc(sizeof(struct nodo)); // Inicializa la variable temporal
@@ -52,18 +59,26 @@ void add(struct nodo *current, int valor)
     temp->data = valor;    // En la temporal asigno el valor
     temp->nextNodo = NULL; // Como va a ser el ultimo nodo de la LinkedList no apunta a nada
 
-    while (current->nextNodo != NULL) // Recorro todo el LinkedList hasta encontrar un enlace que sea NULL
+    if ((*current) == NULL)
     {
-        current = current->nextNodo; // Una vez encontrado, asigna el nodo al nodo que tiene el link NULL
+        (*current) = (struct nodo *)malloc(sizeof(struct nodo));
+        (*current) = temp;
     }
-    current->nextNodo = temp; // A ese nodo con el link NULL, le cambio el enlace por la direccion de la variable temporal que ya tiene los datos
-
+    else
+    {
+        while ((*current)->nextNodo != NULL) 
+        {
+            (*current) = (*current)->nextNodo; 
+        }
+        (*current)->nextNodo = temp; 
+       
+    }
 }
 void pop(struct nodo **current)
 {
     if ((*current) != NULL)
     {
-        printf("%s%d\n","Valor popeado: ",(*current)->nextNodo->data);
+        printf("%s%d\n", "Valor popeado: ", (*current)->data);
         *current = (*current)->nextNodo;
     }
     else
@@ -77,9 +92,9 @@ void printQueue(struct nodo *Stack)
     int pos = 1;
     printf("\n");
     printf("%s", "NULL ");
-    while (Stack != NULL && Stack->nextNodo != 0) // Mientras que haya nodos
+    while (Stack != NULL) // Mientras que haya nodos
     {
-        printf("<--- %d ", Stack->nextNodo->data); // Muestra la info del nodo
+        printf("<--- %d ", Stack->data); // Muestra la info del nodo
         Stack = Stack->nextNodo;         // Para al siguiente nodo mediante el puntero
     }
 
